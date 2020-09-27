19148b635d9d9aec3e2e8929cab4847fbb2b4bcf
Edgar Espino <edgar.espino@loop.colum.edu>
2020-09-26 19:09:29 -0500

    Completed Max speed

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -6,6 +6,7 @@ public class RigidBodyCharacterController : MonoBehaviour
 
 {
     [SerializeField] private float accelerationForce = 10.0f;
+    [SerializeField] private float maxSpeed = 2;
 
     private new Rigidbody rigidbody;
     private Vector2 input;
@@ -18,7 +19,13 @@ public class RigidBodyCharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
+        if(rigidbody.velocity.magnitude < maxSpeed)
+        {
         rigidbody.AddForce(inputDirection * accelerationForce); // << Multiply by time.DetaTime not needed in FixedUpdate
+        }
+
+        /*rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);*/ // << Using this over rides the Unity phisics 
+        // simulation, ei. The character's gravity is affected by this!
     }
 
     private void Update()
