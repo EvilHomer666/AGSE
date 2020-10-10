32c6dab25c8e72e870766e02650b38c8a27b359c
Edgar Espino <edgar.espino@loop.colum.edu>
2020-10-10 02:06:41 -0500

    Created contextual message controller script and stubbed in ShowMessage()
    WIP

---
+++ Assets/Scripts/ContextuaMessageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContextuaMessageController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TMP_Text messegeText; // TextMesh Pro
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        messegeText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    private void ShowMessage(string message, float duration) // Providing parameters to avoid multiple instances of method
+    {
+        messegeText.text = message;
+    }
+}
