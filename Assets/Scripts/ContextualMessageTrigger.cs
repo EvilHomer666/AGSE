0848b22bbf2ae88e40e86d73c847c3ba04a5ca02
Edgar Espino <edgar.espino@loop.colum.edu>
2020-10-10 03:39:47 -0500

    Added event handler, subscribe and unsubscribe

--- Assets/Scripts/ContextualMessageTrigger.cs
+++ Assets/Scripts/ContextualMessageTrigger.cs
@@ -6,13 +6,13 @@ public class ContextualMessageTrigger : MonoBehaviour
 {
     public delegate void ContextualMessageTriggeredAction(); // Can only store void methods with no parameters
 
-    public static event ContextualMessageTriggeredAction ContextualMessageTriggered; // Static
+    public static event ContextualMessageTriggeredAction ContextualMessageTriggered; // Static (need no reference) event to observe
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Anounce when the event occurs
+            // Anounce/Broadcast when the event occurs
             if(ContextualMessageTriggered != null)
             {
             ContextualMessageTriggered.Invoke();
