758198efa1e816aa320f224890db7b9154387b77
Edgar Espino <edgar.espino@loop.colum.edu>
2020-10-10 02:41:50 -0500

    Set up basic coroutine for the contextualMessage show/hid

--- Assets/Scripts/ContextuaMessageController.cs
+++ Assets/Scripts/ContextuaMessageController.cs
@@ -14,10 +14,18 @@ public class ContextuaMessageController : MonoBehaviour
         messegeText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine (ShowMessage("Testing", 2));
     }
 
-    private void ShowMessage(string message, float duration) // Providing parameters to avoid multiple instances of method
+    private IEnumerator ShowMessage(string message, float duration) //Co-routine Providing parameters to avoid multiple instances of method
     {
+        canvasGroup.alpha = 1;
         messegeText.text = message;
+        // Wait for duration
+        yield return new WaitForSeconds(duration);
+        canvasGroup.alpha = 0;
     }
+
+
 }
