6c1c9606dc2d1c7b04ff7eaf948c272a21556537
Edgar Espino <edgar.espino@loop.colum.edu>
2020-11-30 02:51:04 -0600

    Refactored our character controller to clarify

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,6 +14,7 @@ public class RigidBodyCharacterController : MonoBehaviour
     private new Rigidbody rigidbody;
     private Vector2 input;
     private new Collider collider;
+    private float turnSpeed = 0.1f;
 
     private void Start()
     {
@@ -22,40 +24,68 @@ public class RigidBodyCharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vector3 cameraRelativeInputDirection = GetCameraRelativeInputDirection();
+        UpdatePhysicsMaterial();
+        Move(cameraRelativeInputDirection);
+        RotateToFaceMoveInputDirection(cameraRelativeInputDirection);
+    }
+
+    /// <summary>
+    /// Turn the character to face the direction it wants to move in.
+    /// </summary>
+    /// <param name="movementDirection">The direction the character is trying to move in.</param>
+    private void RotateToFaceMoveInputDirection(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude > 0)
+        {
+            var targetRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Moves the player in a direction based on its max speed and acceleration.
+    /// </summary>
+    /// <param name="moveDirection">The direction to move in.</param>
+    private void Move(Vector3 moveDirection)
+    {
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(moveDirection * accelerationForce, ForceMode.Acceleration); // << Multiply by time.DetaTime not needed in FixedUpdate
+        }
+    }
+
+    /// <summary>
+    /// Updtes physics material to a low friction otion if the player is trying to move, or
+    /// a higher friction option if they are trying to stop.
+    /// </summary>
+    private void UpdatePhysicsMaterial()
+    {
+        collider.material = input.magnitude > 0 ? movePhysicsMaterial : stopPhysicsMaterial;
+    }
+
+    /// <summary>
+    /// Uses the input vector to create a camera realtive version
+    /// so the player can move based on the camera's forwardl
+    /// </summary>
+    /// <returns>Returns the camera relative input direction.</returns>
+    private Vector3 GetCameraRelativeInputDirection()
+    {
         var inputDirection = new Vector3(input.x, 0, input.y);
-
-
-        // There is a shorcut syntax for this block of code - See below using the TERNARY OPERATOR or also called conditional operator.
-        //if (inputDirection.magnitude > 0)
-        //{
-        //    collider.material = movePhysicsMaterial;
-        //}
-        //else
-        //{
-        //    collider.material = stopPhysicsMaterial;
-        //}
 
         Vector3 cameraFlattenedForward = Camera.main.transform.forward;
         cameraFlattenedForward.y = 0;
         var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
 
-        Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
-
-        collider.material = inputDirection.magnitude > 0 ? movePhysicsMaterial : stopPhysicsMaterial;
-        // The line of code above does the same as the comented if condition before. Do not abuse this thought!
-
-
-
-        if(rigidbody.velocity.magnitude < maxSpeed)
-        {
-        rigidbody.AddForce(cameraRelativeInputDirection * accelerationForce, ForceMode.Acceleration); // << Multiply by time.DetaTime not needed in FixedUpdate
-        }
-
-        /*rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);*/ // << Using this over rides the Unity phisics 
-        // simulation, ie. The character's gravity is affected by this! Do not use.
+        Vector3 cameraRelativeInputDirectionToReturn = cameraRotation * inputDirection;
+        return cameraRelativeInputDirectionToReturn;
     }
 
-    // Event handler for new input system input
+    /// <summary>
+    /// This event handler is called from the PlayerInput component
+    /// using the new Input system.
+    /// </summary>
+    /// <param name="context"> Vector 2 representing move input.</param>
     public void OnMove(InputAction.CallbackContext context) // Set to public in order to use Invoke Unity Events and Inspectore interface
     {
         input = context.ReadValue<Vector2>();
